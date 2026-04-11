7364a200f50682ed94c05a3b1d53b4fdd72a7931
shwetakannojiya <shwetaaa8813@gmail.com>
2026-04-11 20:05:00 +0530

    User input in python

---
+++ userinput.py
@@ -0,0 +1,7 @@
+# program to take input  from the user
+
+print("A simple program to understand the input method in python")
+
+name= input("Enter your good name here :")
+
+print("your name is :", name)
